0c8bec1019aad3e3435e323b9a912463d541f18d
hightouch <hightouch@hotmail.fr>
2018-10-24 12:59:17 +0200

    ja

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-app.get('/', function(httpRequest, httpResponse) {
-    httpResponse.send('Hello, World!');
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying
+// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
+// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
+// use originWhitelist instead.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
+
+// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
+var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+
+var cors_proxy = require('./lib/cors-anywhere');
+cors_proxy.createServer({
+  originBlacklist: originBlacklist,
+  originWhitelist: originWhitelist,
+  requireHeader: ['origin', 'x-requested-with'],
+  checkRateLimit: checkRateLimit,
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-heroku-queue-wait-time',
+    'x-heroku-queue-depth',
+    'x-heroku-dynos-in-use',
+    'x-request-start',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    xfwd: false,
+  },
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
-
-var request = require('request');
-
-
-app.get('/steam/game/:appid', function(httpRequest, httpResponse) {
-    // Calculate the Steam API URL we want to use
-    var url = 'http://store.steampowered.com/api/appdetails?appids=' +
-        httpRequest.params.appid;
-    request.get(url, function(error, steamHttpResponse, steamHttpBody) {
-        httpResponse.setHeader('Content-Type', 'application/json');
-        httpResponse.send(steamHttpBody);
-    });
-});
-
-app.use('/', express.static('public'));
-
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.text());
-
-
-app.listen(port, () => console.log(`Listening on ${port}`));
-console.log('Listening on port ' + port);
